e3e8f4aab4d71b0649abb297558d57436c5546e5
joo-github <162849279+joo-github@users.noreply.github.com>
2024-10-20 12:24:28 +0800

    Add files via upload

---
+++ Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace JonaJob.Models
+{
+    public class Job
+    {
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string Company { get; set; }
+        public string Salary { get; set; }
+        public string Location { get; set; }
+    }
+}
